bb006b4c31f1cd243429dbaa67cf4431644044c4
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-08-20 09:14:42 +0530

    Implement the member-service

---
+++ backend/member-service/src/api/member-rest-controller.ts
@@ -0,0 +1,91 @@
+import express, {RequestHandler} from 'express';
+import cors from 'cors';
+import {
+    deleteMemberById, existsMemberByContactNotId,
+    existsMemberById,
+    findMembers,
+    getMemberById,
+    saveMember,
+    updateMember
+} from "../repository/member-repository";
+import {Member} from "../dto/member";
+import Joi, {ValidationError} from "joi";
+
+export const router = express.Router();
+router.use(cors());
+
+
+const memberValidator:RequestHandler = (req, res, next) =>{
+    const member = req.body as Member;
+    try {
+        Joi.assert(member, Member.SCHEMA, {abortEarly:false});
+        next();
+    } catch (e){
+        if(e instanceof ValidationError) res.status(400).json(e.details);
+    }
+}
+
+
+router.post('/',memberValidator, async (req, res) => {
+    const member = req.body as Member;
+
+    if (await existsMemberById(member._id)){
+        res.status(409).send("NIC already exists");
+        return;
+    }
+    if (await existsMemberByContactNotId(member.contact, member._id)){
+        res.status(409).send("Contact number already associated with another member");
+        return;
+    }
+
+    await saveMember(req.body);
+    res.sendStatus(201);
+});
+
+
+router.patch('/:memberId', memberValidator, async (req, res) => {
+    const member = req.body as Member;
+    member._id = req.params['memberId'];
+
+    if (!await existsMemberById(member._id)){
+        res.status(404).send("Member doesn't exist");
+        return;
+    }
+    if (await existsMemberByContactNotId(member.contact, member._id)){
+        res.status(409).send("Contact number already associated with another member");
+        return;
+    }
+
+    await updateMember(member);
+    res.sendStatus(204);
+});
+
+
+router.delete('/:memberId', async (req, res) => {
+
+    if (!await existsMemberById(req.params.memberId)){
+        res.status(404).send("Member doesn't exist");
+        return;
+    }
+
+    await deleteMemberById(req.params.memberId);
+    res.sendStatus(204);
+});
+
+
+router.get('/:memberId', async (req, res) => {
+    const member = await getMemberById(req.params.memberId);
+
+    if (member){
+        res.json(member);
+    }else{
+        res.status(404).send("Member doesn't exist");
+    }
+});
+
+
+router.get('/', async (req, res) => {
+    const query = req.query.q ?? '';
+    const memberList = await findMembers(query as string);
+    res.json(memberList);
+});
---
+++ backend/member-service/src/main.ts
@@ -0,0 +1,24 @@
+import env from 'dotenv';
+import {Db, MongoClient} from "mongodb";
+import express, {json} from 'express';
+import {router as MemberRouter} from './api/member-rest-controller';
+
+env.config();
+export let datasource: Db;
+
+async function main(){
+    const mongoClient = new MongoClient(process.env.APP_DB_URL!);
+    await mongoClient.connect();
+    console.log("Successfully connected with the MongoDB Server");
+    datasource =  mongoClient.db(process.env.APP_DB_NAME)
+}
+
+main().then(value => {
+    const app = express();
+
+    app.use(json());
+    app.use("/api/v1/members", MemberRouter);
+
+    app.listen(process.env.APP_PORT,
+        ()=> console.log(`Server has been created at ${process.env.APP_PORT}`))
+}).catch(console.error);
