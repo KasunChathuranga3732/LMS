72dd7084b84dc4a9b0591a22e2da971da11d0399
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-08-21 11:06:43 +0530

    Add routes

--- frontend/lms-angular/src/app/app.module.ts
+++ frontend/lms-angular/src/app/app.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './view/members/members.component';
+import { BooksComponent } from './view/books/books.component';
+import { IssueComponent } from './view/issue/issue.component';
+import { HomeComponent } from './view/home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const route:Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component:HomeComponent
+  },
+  {
+    path: 'books',
+    component:BooksComponent
+  },
+  {
+    path: 'members',
+    component:MembersComponent
+  },
+  {
+    path: 'issues',
+    component:IssueComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersComponent,
+    BooksComponent,
+    IssueComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
