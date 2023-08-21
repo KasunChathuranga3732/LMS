60fb16bea69ecb8d96bb978329cfb575d4193536
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-08-21 09:14:48 +0530

    Initial commit for issue-service

---
+++ backend/issue-service/src/main/java/lk/ijse/dep10/issueservice/IssueServiceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.issueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueServiceApplication.class, args);
+	}
+
+}
---
+++ backend/issue-service/src/test/java/lk/ijse/dep10/issueservice/IssueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.issueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
