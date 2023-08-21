6f4a36e37d3f3fc7547d60a2fde417ad613b825c
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-08-21 12:39:05 +0530

    Add modal window to member component

--- frontend/lms-angular/src/app/view/members/members.component.ts
+++ frontend/lms-angular/src/app/view/members/members.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-members',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent {
+
+
+  newMember(txtId: HTMLInputElement, txtName: HTMLInputElement, txtContact: HTMLInputElement, txtAddress: HTMLInputElement) {
+    // $('#new-customer-modal').removeClass('fade');
+    $('#new-customer-modal').trigger
+    this.resetForm(txtId, txtName, txtContact, txtAddress);
+    setTimeout(()=>txtName.focus(),500)
+  }
+
+  resetForm(txtId:HTMLInputElement, txtName:HTMLInputElement, txtContact:HTMLInputElement, txtAddress:HTMLInputElement){
+    [txtId, txtName, txtAddress, txtContact].forEach(txt => {
+      txt.classList.remove('is-invalid', 'animate__shakeX')
+      txt.value = '';
+    })
+  }
+
 
 }
