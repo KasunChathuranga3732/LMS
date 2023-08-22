5a217accb0c7e77f4063385601dac13a90cf5e8e
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-08-22 09:21:07 +0530

    Add validation logic for the member

--- frontend/lms-angular/src/app/view/members/members.component.ts
+++ frontend/lms-angular/src/app/view/members/members.component.ts
@@ -20,14 +20,14 @@ export class MembersComponent {
   newMember(txtId: HTMLInputElement, txtName: HTMLInputElement, txtContact: HTMLInputElement, txtAddress: HTMLInputElement) {
     // $('#new-customer-modal').removeClass('fade');
     $('#new-customer-modal').trigger
-    this.resetForm(txtId, txtName, txtContact, txtAddress);
+    this.resetForm(txtId, txtName, txtContact, txtAddress, true);
     setTimeout(()=>txtName.focus(),500)
   }
 
-  resetForm(txtId:HTMLInputElement, txtName:HTMLInputElement, txtContact:HTMLInputElement, txtAddress:HTMLInputElement){
+  resetForm(txtId:HTMLInputElement, txtName:HTMLInputElement, txtContact:HTMLInputElement, txtAddress:HTMLInputElement, flag:boolean){
     [txtId, txtName, txtAddress, txtContact].forEach(txt => {
       txt.classList.remove('is-invalid', 'animate__shakeX')
-      txt.value = '';
+      if(flag) txt.value = '';
     })
   }
 
@@ -38,5 +38,53 @@ export class MembersComponent {
         const index = this.memberList.findIndex(member => member.id == id);
         this.memberList.splice(index, 1);
       })
+  }
+
+  saveMember(txtId: HTMLInputElement, txtName: HTMLInputElement, txtAddress: HTMLInputElement, txtContact: HTMLInputElement) {
+    const id = txtId.value;
+    const name = txtName.value;
+    const address = txtAddress.value;
+    const contact = txtContact.value;
+    this.validateData(txtId, txtName, txtContact, txtAddress);
+
+
+  }
+
+  validateData(txtId: HTMLInputElement, txtName: HTMLInputElement, txtContact: HTMLInputElement, txtAddress: HTMLInputElement){
+    const name = txtName.value.trim();
+    const address = txtAddress.value.trim();
+    const contact = txtContact.value.trim();
+    let valid = true;
+    this.resetForm(txtId, txtName, txtContact, txtAddress, false);
+
+    if(!address){
+      valid = this.invalidate(txtAddress, "Address can't be empty")
+
+    } else if(!/^.{3,}$/.test(address)){
+      valid = this.invalidate(txtAddress, "Invalid address")
+    }
+
+    if(!contact){
+      valid = this.invalidate(txtContact, "Contact can't be empty")
+
+    } else if(!/^\d{3}-\d{7}$/.test(contact)){
+      valid = this.invalidate(txtContact, "Invalid Contact number")
+    }
+
+    if(!name){
+      valid = this.invalidate(txtName, "Name can't be empty")
+
+    } else if(!/^[A-za-z ]+$/.test(name)){
+      valid = this.invalidate(txtName, "Invalid name")
+    }
+    return valid;
+  }
+
+
+  invalidate(txt: HTMLInputElement, msg: string){
+    setTimeout(()=>txt.classList.add('is-invalid', 'animate__shakeX'),0);
+    txt.select();
+    $(txt).next().text(msg);
+    return false;
   }
 }
