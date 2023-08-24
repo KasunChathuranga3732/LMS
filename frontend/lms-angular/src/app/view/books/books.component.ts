6ec822af01d84a758858b8d873f7274955732d2b
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-08-24 07:42:52 +0530

    Implement feature to get all books

--- frontend/lms-angular/src/app/view/books/books.component.ts
+++ frontend/lms-angular/src/app/view/books/books.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Book} from "../../dto/book";
+import {HttpClient} from "@angular/common/http";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-books',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent {
-  
+  bookList: Array<Book> = [];
+  btnText: string = 'Save Book';
+  API_BASE_URL: string = 'http://localhost:8080/api/v1/books';
+
+  constructor(private http:HttpClient, private toastr: ToastrService) {
+    http.get<Array<Book>>(`${this.API_BASE_URL}`)
+      .subscribe(memberList => {
+        this.bookList = memberList
+      }, (err) => {
+        this.toastr.error("Can't fetch books: "+err.statusText, 'Error');
+      });
+  }
 }
