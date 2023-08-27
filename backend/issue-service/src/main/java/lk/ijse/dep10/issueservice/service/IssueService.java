4625ba0d90be23d1f4bf001fd248818b00b2510c
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-08-27 10:18:05 +0530

    Implement IssueService interface

---
+++ backend/issue-service/src/main/java/lk/ijse/dep10/issueservice/service/IssueService.java
@@ -0,0 +1,18 @@
+package lk.ijse.dep10.issueservice.service;
+
+import lk.ijse.dep10.issueservice.dto.IssueDTO;
+
+import java.util.List;
+
+public interface IssueService {
+
+    void saveIssue(IssueDTO issue);
+
+    void updateIssue(IssueDTO issue);
+
+    void deleteIssue(IssueDTO issue);
+
+    List<IssueDTO> findIssues(String query);
+
+
+}
