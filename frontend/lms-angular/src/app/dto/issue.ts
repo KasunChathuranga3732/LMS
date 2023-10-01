574ed01ca0a4131608bfabe074940c78352d7180
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-10-01 09:07:35 +0530

    Implement logic to display issueList in the table

--- frontend/lms-angular/src/app/dto/issue.ts
+++ frontend/lms-angular/src/app/dto/issue.ts
@@ -1,6 +1,7 @@
 export class Issue {
 
   constructor(public id:number|null, public isbn:string, public memberId:string, public issueDate:Date,
-              public dueDate:Date, public fine:number, public retuned:string) {
+              public returnDate:Date, public fine:number, public returned:string) {
+    this.fine = parseFloat(this.fine.toFixed(2));
   }
 }
--- frontend/lms-angular/src/app/view/issue/issue.component.ts
+++ frontend/lms-angular/src/app/view/issue/issue.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {Issue} from "../../dto/issue";
+import {DatePipe} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {ToastrService} from "ngx-toastr";
+import {Book} from "../../dto/book";
 
 @Component({
   selector: 'app-issue',
@@ -6,5 +11,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./issue.component.scss']
 })
 export class IssueComponent {
+  issueList: Array<Issue> = [];
+  API_BASE_URL: string = 'http://localhost:8080/api/v1/issues';
+  return: string = 'All';
+  id =  null;
+  isbn: string = '';
+  memberId: string = '';
+  pipe = new DatePipe('en-US');
+  issueDate = this.pipe.transform(new Date(), 'yyyy-MM-dd');
+  dueDate = this.pipe.transform(new Date().setDate(new Date().getDate() + 10), 'yyyy-MM-dd');
+
+  constructor(private http:HttpClient, private toastr: ToastrService) {
+    http.get<Array<Issue>>(`${this.API_BASE_URL}`)
+      .subscribe(issueList => {
+        this.issueList = issueList
+        console.log(issueList);
+      }, (err) => {
+        this.toastr.error("Can't fetch issues: " + err.statusText, 'Error');
+      });
+  }
 
 }
