59c2b1c6be4884251eb94d589d340fb74bfcc6a3
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-10-04 08:54:21 +0530

    Implement logic to get total issues

--- frontend/lms-angular/src/app/view/home/home.component.ts
+++ frontend/lms-angular/src/app/view/home/home.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {ToastrService} from "ngx-toastr";
 import {Member} from "../../dto/member";
 import {Book} from "../../dto/book";
+import {Issue} from "../../dto/issue";
 
 @Component({
   selector: 'app-home',
@@ -16,6 +17,7 @@ export class HomeComponent {
   totalFines: string = '';
   availableBooks: string = '';
   issuedBooks: string = '';
+  issueList: Array<Issue> = [];
   API_BASE_URL_ISSUE: string = 'http://localhost:8080/api/v1/issues';
   API_BASE_URL_BOOK: string = 'http://localhost:8080/api/v1/books';
   API_BASE_URL_MEMBER: string = 'http://localhost:8080/api/v1/members';
@@ -23,6 +25,7 @@ export class HomeComponent {
   constructor(private http:HttpClient, private toastr: ToastrService) {
     this.getMembers()
     this.getBooks()
+    this.getIssues()
   }
 
   getMembers(){
@@ -44,6 +47,16 @@ export class HomeComponent {
         this.totalBooks = '' + count;
       }, (err) => {
         this.toastr.error("Can't fetch books: " + err.statusText, 'Error');
+      })
+  }
+
+  getIssues(){
+    this.http.get<Array<Issue>>(`${this.API_BASE_URL_ISSUE}`)
+      .subscribe(issueList => {
+        this.issueList = issueList;
+        this.totalIssues = '' + issueList.length
+      }, (err) => {
+        this.toastr.error("Can't fetch members: " + err.statusText, 'Error');
       })
   }
 
